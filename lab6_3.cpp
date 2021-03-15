bae60dc196254e4b7f99905d6b9ba85ed0c2978f
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 18:42:24 +0700

    ...

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -3,11 +3,11 @@ int main()
 {
 	int N, Q;
 	scanf("%d %d", &N, &Q);
-	int a[N];
+	int a[N] = {};
 	char cmd;
 	int ncmd[2];
 	return 0;
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < Q; i++)
 	{
 		scanf("%c %d %d", &cmd, &ncmd[0], &ncmd[1]);
 		if (cmd == 'U')
