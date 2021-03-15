2dd7b41f29102c5472c36bb7f640c1b5035f4976
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 18:40:00 +0700

    ...

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -12,7 +12,19 @@ int main()
 		scanf("%c %d %d", &cmd, &ncmd[0], &ncmd[1]);
 		if (cmd == 'U')
 		{
-			
+			a[ncmd[ncmd[0]-1]] = ncmd[1];
+		}
+		else
+		{
+			int most = a[ncmd[0]];
+			for (int i = ncmd[0]+1; i <= ncmd[1]; i++)
+			{
+				if (most < a[i])
+				{
+					most = a[i];
+				}
+			}
+			printf("%d", most);
 		}
 	}
 }
