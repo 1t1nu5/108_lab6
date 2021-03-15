976f89e74b39dd06014bf046e649ccaa38c8d87d
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 16:57:13 +0700

    fixing...

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -6,15 +6,15 @@ int main()
 	scanf("%d", &n);
 	int ans[n], guess[n];
 	scanf("%s", ans);
-	scanf("%s", guess);
+	scanf(" %s", guess);
 	int score = 0;
 	for (int i = 0; i < n; i++)
 	{
-		if (ans[i] = guess[i])
+		if (ans[i] == guess[i])
 		{
 			score++;
 		}
 	}
-	printf("%d %d", score, n-score);
+	printf("%d %d", score+1, n-score-1);
 	return 0;
 }
