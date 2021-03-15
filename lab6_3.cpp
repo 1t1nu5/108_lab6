8bd0751c4b3bdefcd99f63c1996b9eb3e6762c04
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 17:16:08 +0700

    set variable and receive

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	
+	int people;
+	scanf("%d", &people);
+	int score[people], love[people][2];
+	for (int i = 0; i < people; i++)
+	{
+		scanf("%d %d", &love[i][0], &love[i][1]);
+	}
 	return 0;
 }
