37e7f579eb1a5d7e54dff27d022f9abd6314273e
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 17:19:20 +0700

    add first variable

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -10,12 +10,13 @@ int main()
 		score[love[i][0]]++;
 		score[love[i][i]]++;
 	}
-	int most = score[0];
+	int most = score[0], first = 0;
 	for (int i = 1; i < people; i++)
 	{
 		if (most < score[i])
 		{
 			most = score[i];
+			first = i;
 		}
 	}
 	return 0;
