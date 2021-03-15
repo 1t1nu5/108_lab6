c257d387150dda250f5bf2baa1def07333f3076f
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 18:26:24 +0700

    ตั้งชื่อไฟล์สลับกัน

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -1,24 +1,6 @@
 #include <stdio.h>
 int main()
 {
-	int people;
-	scanf("%d", &people);
-	int score[people] = {}, love[people][2];
-	for (int i = 0; i < people; i++)
-	{
-		scanf("%d %d", &love[i][0], &love[i][1]);
-		score[love[i][0]]++;
-		score[love[i][i]]++;
-	}
-	int most = score[0], first = 0;
-	for (int i = 1; i < people; i++)
-	{
-		if (most < score[i])
-		{
-			most = score[i];
-			first = i;
-		}
-	}
-	printf("%d", first);
+	
 	return 0;
 }
