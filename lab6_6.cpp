318b52b4c49c098effaa2ecf1861c62d05dabdc3
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 17:29:59 +0700

    add variable and preset

--- lab6_6.cpp
+++ lab6_6.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main()
 {
-	
+	int side;
+	scanf("%d", side);
+	char paper[side][side];
+	for (int i = 0; i < side; i++)
+	{
+		for (int j = 0; j < side; j++)
+		{
+			paper[side][side] = '.';
+		}
+	}
 	return 0;
 }
