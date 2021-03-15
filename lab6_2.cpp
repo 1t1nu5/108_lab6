ac6fdf6cb9d80b9ef9395cf0e1955cfed5e29efc
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-03-15 16:49:10 +0700

    add variable and receiving

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	
+	int n;
+	scanf("%d", &n);
+	int ans[n], guess[n];
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s", ans);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s", guess);
+	}
 	return 0;
 }
